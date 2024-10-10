b40a41eeb67343bc2567f6fa02941107f7fbb1e9
Ahsanul Haque Nehal <007cryptic@gmail.com>
2024-10-10 22:38:46 +0600

    card deign complete

--- src/assets/Components/Card.jsx
+++ src/assets/Components/Card.jsx
@@ -1,15 +1,19 @@
-export default function Card({ name }) {
+/* eslint-disable react/prop-types */
+export default function Card({ data, name, weight, height, image }) {
   return (
     <>
-      <div className="bg-[#2C3E49] w-[18rem] flex flex-col justify-center items-center px-6 pb-[18px] pt-[8px] gap-3 rounded-lg text-white">
-        <img src={""} alt="image" />
-        <h2 className="text-[30px] font-bold">{name}</h2>
+      <div className="bg-[#2C3E49] w-[18rem] h-[22rem] flex flex-col justify-center items-center px-6 pb-[18px] pt-[8px] gap-3 text-white rounded-lg">
+        <img src={image} alt="image" className="max-h-[190px]" />
+        <h2 className="text-[30px] text-green-500 font-bold">{name}</h2>
+        <div className="flex gap-2 font-bold underline">
+          <p>{data.types.map((curElem) => curElem.type.name).join(", ")}</p>
+        </div>
         <div className="w-full flex justify-between">
           <p>
-            height : <span className="font-bold">{}</span>
+            height : <span className="font-bold">{height}</span>
           </p>
           <p>
-            Weight : <span className="font-bold">{}</span>
+            Weight : <span className="font-bold">{weight}</span>
           </p>
           <p>
             Speed : <span className="font-bold">{}</span>
--- src/assets/pages/Pokemon.jsx
+++ src/assets/pages/Pokemon.jsx
@@ -3,9 +3,13 @@ import { useEffect, useState } from "react";
 
 export default function Pokemon() {
   const [apiData, setApiData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  // Pokemon API
   const API = "https://pokeapi.co/api/v2/pokemon?limit=124";
 
+  // Get data form API
   const getApiData = async () => {
     try {
       const res = await fetch(API);
@@ -18,14 +22,33 @@ export default function Pokemon() {
 
       const detailedResponse = await Promise.all(detailData);
       setApiData(detailedResponse);
+      setLoading(false);
     } catch (error) {
       console.log(error.message);
+      setLoading(false);
+      setError(error);
     }
   };
-
+  // handle useEffect for fetch data in first render
   useEffect(() => {
     getApiData();
   }, []);
+
+  // handle loading
+  if (loading)
+    return (
+      <div className="flex justify-center items-center text-[50px] mt-[5rem]">
+        <h1 className="text-green-500 font-bold">Loading</h1>
+      </div>
+    );
+
+  if (error) {
+    return (
+      <div className="flex justify-center items-center text-[50px] mt-[5rem]">
+        <h1 className="text-white font-bold">{error.message}</h1>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col justify-center items-center mt-8 gap-4">
@@ -34,13 +57,19 @@ export default function Pokemon() {
       </h1>
       <input
         type="text"
-        className="px-4 text-[20px] py-1 outline-none border-b-2 border-gray-500"
+        className="px-4 text-[20px] py-1 outline-none border-b-2 border-green-500"
       />
       <div className="mt-4">
         <ul className="flex flex-wrap justify-center items-center gap-8">
-          {apiData.map((value, index) => (
-            <li key={index}>
-              <Card name={value.name} />
+          {apiData.map((value) => (
+            <li key={value.id}>
+              <Card
+                data={value}
+                name={value.name}
+                weight={value.weight}
+                height={value.height}
+                image={value.sprites.other.dream_world.front_default}
+              />
             </li>
           ))}
         </ul>
